d2b8c340a7d9c68b6c5ba9b793ef9935ed69f6dc
Joca0 <137853850+Joca0@users.noreply.github.com>
2026-08-10 13:07:06 -0300

    Initial commit

---
+++ api/src/test/java/com/conectaedu/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conectaedu.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
